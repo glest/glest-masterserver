082c28adfbb3cb974d0954f6352d693197ab8a0f
andy5995 <andy400-dev@yahoo.com>
2018-01-17 14:32:42 -0600

    fixes side effect from 99d86de

--- functions.php
+++ functions.php
@@ -3,6 +3,10 @@
 //      The MegaGlest Team, under GNU GPL v3.0
 // ==============================================================
 
+class Registry {
+    static public $mysqliLink = null;
+}
+
 	if ( !defined('INCLUSION_PERMITTED') || ( defined('INCLUSION_PERMITTED') && INCLUSION_PERMITTED !== true ) ) { die( 'This file must not be invoked directly.' ); }
 
 	# This function cleans out special characters
@@ -13,7 +17,7 @@
 		//$code_entities_replace = array('','','','','','','','','','','','','','','','','','','','','');
 		$code_entities_match   = array('$','%','^','&','_','+','{','}','|','"','<','>','?','[',']','\\',';',"'",'/','+','~','`','=');
 		$code_entities_replace = array('','','','','','','','','','','','','');
-        
+
 		$text = str_replace( $code_entities_match, $code_entities_replace, $text );
 		return $text;
 	}
@@ -25,7 +29,7 @@
 		}
 
 		// If we may use persistent MYSQL database server links...
-		if ( defined( 'MYSQL_LINK_PERSIST' ) && MYSQL_LINK_PERSIST === true ) 
+		if ( defined( 'MYSQL_LINK_PERSIST' ) && MYSQL_LINK_PERSIST === true )
 		{
 			// ...then reuse an existing link or create a new one, ...
 			$linkid = mysqli_pconnect( MYSQL_HOST, MYSQL_USER, MYSQL_PASSWORD );
@@ -50,8 +54,8 @@
 
 	function get_localsubnet_ip_prefix()
 	{
-		// If this is supposed to match any RFC1918 or even any unroutable IP address space then this is far from complete. 
-		// Consider using something like the 'IANA Private list' provided at http://sites.google.com/site/blocklist/ instead. 
+		// If this is supposed to match any RFC1918 or even any unroutable IP address space then this is far from complete.
+		// Consider using something like the 'IANA Private list' provided at http://sites.google.com/site/blocklist/ instead.
 		// The data in this list is a subset of what IANA makes available at http://www.iana.org/assignments/ipv4-address-space/ipv4-address-space.xhtml
 		// Note that you may want/need to add in the IPv6 equivalent, too: http://www.iana.org/assignments/ipv6-address-space/ipv6-address-space.xhtml
 		return "192.";
